393c951ac9c9aad59cdd8e22088b2b19b9aba649
marmiha <miha.markez@gmail.com>
2020-11-06 21:01:43 +0100

    Code refractoring

--- handlers/users.go
+++ handlers/users.go
@@ -8,13 +8,13 @@ import (
 
 func (s *Server) registerUser(writer http.ResponseWriter, request *http.Request) {
 	// This is the payload that we need.
-	payload := new(domain.RegisterPayload)
+	var payload domain.RegisterPayload
 
 	// Call the validate payload.
 	next := validatePayload(func(writer http.ResponseWriter, request *http.Request) {
 		// If successful then this will be executed.
 		fmt.Println(payload)
-	}, payload)
+	}, &payload)
 
 	next.ServeHTTP(writer, request)
 }
